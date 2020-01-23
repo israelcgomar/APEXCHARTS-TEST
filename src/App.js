313a75a45f2d35e28d8dd1c2227b8e85db160064
Israel Castillo Gomar <icastillog@teambyte.com.mx>
2020-01-23 16:34:55 -0600

    ! update chart lybrary

--- src/App.js
+++ src/App.js
@@ -1,62 +1,51 @@
 import React from 'react';
-import ReactApexChart from 'react-apexcharts';
+import { Line } from 'react-chartjs-2'
+import 'chartjs-plugin-streaming'
+
 
 import './App.css';
 
+const data = {
+  datasets: [
+    {
+      label: "MEMORIA RAM",
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+      lineTension: 0,
+      borderDash: [6, 4],
+      data: []
+    }
+  ]
+};
 
+const options = {
+  scales: {
+    xAxes: [
+      {
+        type: "realtime",
+        realtime: {
+          onRefresh: function() {
+            data.datasets[0].data.push({
+              x: Date.now(),
+              y: Math.random() * 10
+            });
+          },
+          delay: 3000
+        }
+      }
+    ]
+  }
+};
 class App extends React.Component {
 
   constructor(props) {
     super(props);
 
+   
+
     this.state = {
-      series: [{
-        name: 'series1',
-        data: Array.from({length: 8}, () => Math.floor(Math.random() * 40))
-      }],
-      options: {
-        chart: {
-          fontFamily: 'Helvetica, Arial, sans-serif',
-          height: 350,
-          type: 'area',
-        },
-        animations: {
-          enabled: true,
-          easing: 'easeinout',
-          speed: 800,
-          animateGradually: {
-            enabled: true,
-            delay: 150
-          },
-          dynamicAnimation: {
-            enabled: true,
-            speed: 350
-          }
-        },
-        dataLabels: {
-          enabled: false
-        },
-        stroke: {
-          curve: 'smooth'
-        },
-        xaxis: {
-          type: 'datetime',
-          categories: [
-            "2020-01-19T00:00:00.000Z",
-            "2020-01-19T01:30:00.000Z",
-            "2020-01-19T02:30:00.000Z",
-            "2020-01-19T03:30:00.000Z",
-            "2020-01-19T04:30:00.000Z",
-            "2020-01-19T05:30:00.000Z",
-            "2020-01-19T06:30:00.000Z"
-          ]
-        },
-        tooltip: {
-          x: {
-            format: 'dd/MM/yy HH:mm'
-          },
-        },
-      },
+ 
+      
     };
   }
 
@@ -64,30 +53,23 @@ class App extends React.Component {
 
   componentDidMount() {
 
-
-    
-    window.setInterval(() => {
-
-    
-    
-          const data =  Array.from({length: 8}, () => Math.floor(Math.random() * 40))
-          console.log(data);
-          
-   
-       
-      
-        
-      
-    }, 5000)
   }
       
 
   render() {
+    
+   
+    
     return (
       <div className="App">
         <header className="App-header">
 
-          <ReactApexChart options={this.state.options} series={this.state.series} type="area" height={650} width={1050} />
+        <Line 
+          data={data} 
+          options={options} 
+          width={100}
+          height={50}
+        />
 
 
         </header>
